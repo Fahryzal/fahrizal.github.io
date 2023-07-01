cb85aaf947fd446645ceae3bcd8f968f1b5a1b71
Fahrizal <fahryzalsagala@gmail.com>
2023-07-01 14:18:59 +0700

    menambahkan avatar.png

--- script.js
+++ script.js
@@ -9,5 +9,8 @@ const circle = document.querySelector('.circle');
 
 circle.addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
+    circle.classList.toggle('circle');
+    circle.classList.toggle('circle2');
 })
+
 
