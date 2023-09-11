333123e0f291f0d1da51e67ffc5ccd6483e26474
Fahrizal <fahryzalsagala@gmail.com>
2023-09-11 19:31:51 +0700

    nav active link

--- script.js
+++ script.js
@@ -31,5 +31,26 @@ document.addEventListener('click', function (e) {
 });
 
 
+const sections = document.querySelectorAll("section[id]");
+console.log(sections)
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(".shui a[href*=" + sectionId + "]");
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
+
 
 
